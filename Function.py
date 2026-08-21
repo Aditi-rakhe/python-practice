9c67c9a37d0b9268038617a0e8bd919237cd18de
Aditi-rakhe <rakheaditi0@gmail.com>
2026-08-21 01:55:47 -0700

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,16 @@
+# Write a function to add two numbers.
+def add(a,b):
+    return a +b
+print(add(10,20))
+
+# Check whether a number is even or odd
+def even_odd(num):
+    if num%2 == 0:
+        return"even"
+    else :
+        return "odd"
+print(even_odd(8))
+
+# Find the largest of two numbers
+def  largest(a,b):
+    if a>b :
