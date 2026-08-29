b3cac23cbc78cbb8fd0eee0f468145886a33ba18
Aditi-rakhe <rakheaditi0@gmail.com>
2026-08-29 02:01:19 -0700

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,85 @@
+# 1.Create variables name, age, and city. Print their values.
+# name = "Aditi"
+# age = "22"
+# city = "pune"
+
+# print(name)
+# print(age)
+# print(city)
+
+# # 2 store your first name and last name in two variables and print the full name.
+# first_name = "Aditi"
+# last_name = "Rakhe"
+
+# full_name = first_name + " " + last_name
+
+# print(first_name)
+# print(last_name)
+
+# # 3. Create variables for product name, price, and quantity. Calculate and print the total bill.
+# p_name = "bag"
+# p2 = 500
+# q1=2 
+
+# Total_bill= p2* q1
+
+# print ("p_name",p_name)
+# print("total bill :",Total_bill)
+
+# # 4. Store two numbers in variables and print their addition, subtraction, multiplication, and division.
+# p1 = 355
+# p2= 233
+
+# print ("Addition:",p1+p2)
+# print ("substraction:",p1- p2)
+# print ("multiplication :",p1*p2)
+# print("division:",p1/p2)
+
+# 5. Create a variable and use type() to display its data type.
+
+# salary = 25000
+
+# print(type(salary))
+
+# name = "Aditi"
+
+# print(type(name))
+
+# 6. Store your age in a variable and calculate your approximate birth year.
+# age = 22
+# current_year = 2026
+
+# birth_year = current_year - age
+
+# print("Approximate birth year :" ,birth_year)
+
+# 7. Store temperature in Celsius and convert it to Fahrenheit using variables.
+# celsius = 26
+# ft = (celsius*9/5)+12
+# print("temoerature in ft:",ft)
+
+#.8 Store marks of three subjects in variables and calculate total and average.
+# Maths = 30
+# science = 40
+# English = 35
+# Total = Maths +science +English
+# average= Total /3
+
+# print("Total marks :",Total)
+# print("Average marks :", average)
+
+# 9. Swap the values of two variables without using a third variable.
+# a= 10
+# b= 20
+
+# a,b = b,a
+
+# print("a =",a)
+# print("b =",b)
+
+# 10. Take a user's name and age using input(), store them in variables, and display a welcome message.
+name = input("Enter your name ")
+age = input ("Enter your age :")
+
+print("welcome,name")
+print("Ypur age is 22",age)
