b16cc0f28bd16934e6302539801b7df0c56ed321
Aditi-rakhe <rakheaditi0@gmail.com>
2026-09-01 03:47:15 -0700

    Add files via upload

---
+++ qyestions1.py
@@ -0,0 +1,68 @@
+# # create a list of 5 numbers and print the list
+# v1 = [120,30,4,50]
+# # Create a list of 5 fruits and print each fruit.
+# r1= ["apple","orange","mango","straberry"]
+# for fruit  in r1:
+#     print(fruit)
+# # Create a list of numbers and find its length.
+# e1= [1,2,3,4,5,6,7,8,9,9,9]
+# print(len(e1))
+# # Create a list and print the first element and last element
+# v1 = [10,2,30,40,50,70,80,90]
+# print(v1[0])
+# print(v1[-1])
+# # Create a list of 5 numbers and find their sum
+# A1=[10,11,22,45,66]
+# sum = 0
+# for i in A1:
+#     sum = sum+i
+
+# print(sum)
+# Create a list of numbers and find the maximum and minimum values.
+# B1 = [3,9,11,12,14,18,26,30,35]
+
+# print(min(B1))
+# print(max(B1))
+# Create a list of numbers and calculate the average.
+# n1 =[10,20,30,40,5,60,80,90]
+# Average = sum(n1) / len(n1)
+# print(Average)
+# Create a list and check whether a particular element exists in the list.
+# F1 = [10,20,30,40,60,80,90]
+
+# print(30 in F1)
+# Create a list and use append() to add an element.
+# A = [10,30,4,50]
+# A.append(40)
+# print(A)
+
+# Create a list and use insert() to add an element at a specific position. 
+# A=[10,20,30,40]
+# A.insert(2,25)
+# print(A)
+
+# # Create a list and use remove() to remove an element.
+# A=[10,20,30,40]
+# A.remove(40)
+# print(A)
+
+# # Create a list and use pop() to remove an element.
+# A=[10,20,30,40]
+# A.pop(3)
+
+# # Create a list and sort numbers in descending order.
+# A=[10,103,118,400]
+# A.sort(reverse = True)
+# print(A)
+
+# Create a list and use reverse() to reverse the list.
+A=[10,20,30,40,50,60,70]
+A.reverse()
+print(A)
+
+
+
+
+
+
+
